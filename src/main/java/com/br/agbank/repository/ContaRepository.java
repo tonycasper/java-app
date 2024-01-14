115c69f222c3cf5b6fe57201a362521f1ef13e56
André Gaspar <andregaspar@Andres-MacBook-Air.local>
2024-01-14 20:50:33 -0300

    add metodo no repository buscar por agencia

--- src/main/java/com/br/agbank/repository/ContaRepository.java
+++ src/main/java/com/br/agbank/repository/ContaRepository.java
@@ -1,8 +1,11 @@
 package com.br.agbank.repository;
 
 import com.br.agbank.model.Cliente;
+import com.br.agbank.model.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContaRepository extends JpaRepository<Cliente, Long> {
-    // Métodos de consulta específicos
+    Optional<Conta> findByAgencia(String agencia);
 }
