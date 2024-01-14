a4bf68a6a7bd17f9feee132e56f8b935f5f8f16a
André Gaspar <andregaspar@Andres-MacBook-Air.local>
2024-01-14 20:47:24 -0300

    add ajuste model Cliente

--- src/main/java/com/br/agbank/model/Cliente.java
+++ src/main/java/com/br/agbank/model/Cliente.java
@@ -3,18 +3,18 @@ package com.br.agbank.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 @Entity
+@Data
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-    private String documento; // CPF ou CNPJ
+    private String cpfCnpj;
     private String endereco;
     private String senha;
-
-    // Construtores, getters e setters
 }
 
