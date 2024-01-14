b04539ca0b1a8f76d35396f5316d44689e25989d
André Gaspar <andregaspar@Andres-MacBook-Air.local>
2024-01-14 20:48:44 -0300

    add metodo no repository

--- src/main/java/com/br/agbank/repository/ClienteRepository.java
+++ src/main/java/com/br/agbank/repository/ClienteRepository.java
@@ -3,6 +3,8 @@ package com.br.agbank.repository;
 import com.br.agbank.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    // Métodos de consulta específicos
+    Optional<Cliente> findByDocumento(String documento);
 }
