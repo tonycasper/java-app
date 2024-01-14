8b83fe3af2b69b87059fef5ead9869c6d3b857c5
André Gaspar <andregaspar@Andres-MacBook-Air.local>
2024-01-14 20:49:44 -0300

    add ajuste model Conta

--- src/main/java/com/br/agbank/model/Conta.java
+++ src/main/java/com/br/agbank/model/Conta.java
@@ -3,18 +3,19 @@ package com.br.agbank.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+import java.math.BigDecimal;
+
 @Entity
+@Data
 public class Conta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
-    private String documento; // CPF ou CNPJ
-    private String endereco;
-    private String senha;
-
-    // Construtores, getters e setters
+    private String agencia;
+    private BigDecimal saldo;
+    private String status; // "Ativa" ou "Inativa"
 }
 
