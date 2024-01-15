7a4b4f38b942362472ce1e9ab5a7820642bc53c5
André Gaspar <andregaspar@Andres-MacBook-Air.local>
2024-01-14 21:28:24 -0300

    add meotodo de enviarNotificacao

--- src/main/java/com/br/agbank/service/TransacaoService.java
+++ src/main/java/com/br/agbank/service/TransacaoService.java
@@ -6,16 +6,26 @@ import com.br.agbank.repository.ContaRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
 public class TransacaoService {
 
     private ContaRepository contaRepository;
+    private RestTemplate restTemplate;
+    private static final String NOTIFICATION_ENDPOINT = "https://run.mocky.io/v3/9769bf3a-b0b6-477a-9ff5-91f63010c9d3";
+    private Logger log;
 
     @Transactional
     public void realizarTransferencia(Long idContaOrigem, Long idContaDestino, BigDecimal valor) {
@@ -40,5 +50,23 @@ public class TransacaoService {
     }
 
     private void enviarNotificacao(Conta contaOrigem, Conta contaDestino, BigDecimal valor) {
+        Map<String, String> notificationDetails = new HashMap<>();
+        notificationDetails.put("contaOrigemId", contaOrigem.getId().toString());
+        notificationDetails.put("contaDestinoId", contaDestino.getId().toString());
+        notificationDetails.put("valorTransferido", valor.toString());
+
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(NOTIFICATION_ENDPOINT, notificationDetails, String.class);
+
+            if (response.getStatusCode() == HttpStatus.OK) {
+                log.info("Notificação enviada com sucesso para a conta de origem ID: " + contaOrigem.getId() + " e conta de destino ID: " + contaDestino.getId());
+//                transacao.setStatus("Notificação Enviada");
+//                transacaoRepository.save(transacao);
+            }
+
+        } catch (RestClientException e) {
+            // Logar o erro e possivelmente tentar reenviar a notificação mais tarde
+        }
     }
+
 }
